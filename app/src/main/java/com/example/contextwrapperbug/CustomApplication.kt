8031a22ee80af39a3fb53c5fd874657e48d24f20
Guillermo Orellana <guillermo.orellana@skyscanner.net>
2019-05-23 11:00:03 +0100

    clarify

--- app/src/main/java/com/example/contextwrapperbug/CustomApplication.kt
+++ app/src/main/java/com/example/contextwrapperbug/CustomApplication.kt
@@ -3,9 +3,9 @@ package com.example.contextwrapperbug
 import android.app.Application
 
 class CustomApplication : Application() {
-    // should return Any?
+    // should return Any?, and not be allowed by the compiler to return Any
     override fun getSystemService(name: String): Any {
-        // if you decompile, there's a
+        // since it returns Any, when you decompile, there's a
         // Intrinsics.checkExpressionValueIsNotNull(var10000, "super.getSystemService(name)");
         // which will make the application crash in case the super returns null (which is a valid case)
         return super.getSystemService(name)
